cc49934e22030c4aa26d60c4f56f2d56540fa19b
Nickbing Lao <giscafer@outlook.com>
2019-09-02 08:44:17 +0000

    master - 11fdb16 release(v0.1.6): release v0.1.6

--- @pixelmon/theme/fesm2015/theme.js
+++ @pixelmon/theme/fesm2015/theme.js
@@ -3502,7 +3502,7 @@ PixelmonThemeModule.ctorParameters = () => [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('0.1.6-37b4269');
+const VERSION = new Version('0.1.6');
 
 /**
  * @fileoverview added by tsickle
